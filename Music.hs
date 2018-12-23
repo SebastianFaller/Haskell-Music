3b29e763215e6c25f8af9d359148efc919969208
Sebastian Faller <sebastian.faller2@student.kit.edu>
2018-12-23 23:30:17 +0100

    Task 2. Implemented Num and Fractional really.

--- Music.hs
+++ Music.hs
@@ -17,3 +17,16 @@ sine f = Signal $ [sin (f*2*pi*x/sampleRate) | x <- [0..]]
 
 trim :: Signal -> Double -> Signal
 trim (Signal d) t = Signal $ take (double2Int $ sampleRate*t)  d
+
+instance Num Signal where
+	(+) (Signal a) (Signal b) = Signal $ zipWith (+) a b
+	(-) (Signal a) (Signal b) = Signal $ zipWith (-) a b  
+	(*) (Signal a) (Signal b) = Signal $ zipWith (*) a b 
+	negate x = 0-x
+	fromInteger = constant . fromInteger 
+instance Fractional Signal where
+	(/) (Signal a) (Signal b) = Signal $ zipWith f a b
+		where f x y = 
+			if y /= 0 then x/y 
+			else 0 
+	fromRational =  constant . fromRational 
